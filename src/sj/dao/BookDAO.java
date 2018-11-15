bdfc6bb6a1ff525e6fd5a2f2f7b0e5bfac568c8f
tune <tune@192.168.3.11>
2018-11-15 22:59:13 +0900

    add BookDAO, BookManager method

--- src/sj/dao/BookDAO.java
+++ src/sj/dao/BookDAO.java
@@ -1,6 +1,7 @@
 package sj.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -17,7 +18,41 @@ public class BookDAO {
 	public  BookDAO() throws DAOException {
 		getConnection();
 	}
+//登録
+		//資料を新規で登録する
+	public BookBean insertBook(long isbn, String name, int catcode, String author, String publisher, Date pubdate,
+			String coverurl) throws DAOException{
+		if (con == null)
+			getConnection();
+			PreparedStatement st = null;
 
+		try {
+			//SQL文の作成
+			String sql = "INSERT INTO bookinfo(isbn, name, catcode, author, publisher, pubdate, coverurl)"
+						+ "VALUES(?, ?, ?, ?, ?, ?, ?)";
+			//PreparedStatementオブジェクトの取得
+			st = con.prepareStatement(sql);
+			//カテゴリの設定
+			st.setLong(1, isbn);
+			st.setString(2, name);
+			st.setInt(3, catcode);
+			st.setString(4, author);
+			st.setString(5, publisher);
+			st.setDate(6, pubdate);
+			st.setString(7, coverurl);
+			//sqlの実行
+			st.executeUpdate();
+
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return null;
+
+	}
+
+
+//検索
 	//本の情報を全件取得
 	//findAllBook
 	public List<BookBean> findAllBook() throws DAOException {
@@ -37,31 +72,75 @@ public class BookDAO {
 			// 結果の取得および表示
 			List<BookBean> list = new ArrayList<BookBean>();
 			while (rs.next()) {
-			BookBean bean = new BookBean(
-									rs.getLong("isbn"),
-									rs.getString("name"),
-									rs.getInt("catcode"),
-									rs.getString("author"),
-									rs.getString("publisher"),
-									rs.getDate("pubdate"),
-									rs.getString("coverurl"))
-					;
-							list.add(bean);
-						}	return list;
-					} catch (SQLException e) {
-						e.printStackTrace();
-						throw new DAOException("レコードの取得に失敗しました。");
-					} finally {
-						try {
-							// リソースの開放
-							if(rs != null) rs.close();
-							if(st != null) st.close();
-							close();
-						} catch (Exception e) {
-							throw new DAOException("リソースの開放に失敗しました。");
-						}
-					}
+				BookBean bean = new BookBean(
+						rs.getLong("isbn"),
+						rs.getString("name"),
+						rs.getInt("catcode"),
+						rs.getString("author"),
+						rs.getString("publisher"),
+						rs.getDate("pubdate"),
+						rs.getString("coverurl"));
+				list.add(bean);
+			}return list;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException("レコードの取得に失敗しました。");
+		}finally{
+			try {
+				// リソースの開放
+				if(rs != null) rs.close();
+				if(st != null) st.close();
+				close();
+			} catch (Exception e) {
+				throw new DAOException("リソースの開放に失敗しました。");
+			}
+		}
 	}
+
+		// isbnで資料検索
+	public List<BookBean> findBookByISBN(long isbn) {
+		return null;
+	}
+
+
+
+	public List<BookBean> findBookByName(String name) {
+		// TODO 名前で検索
+		return null;
+	}
+
+
+
+	public List<BookBean> findBookByauthor(String author) {
+		// 作者で検索
+		return null;
+	}
+
+
+
+	public List<BookBean> findBookBypublisher(String publisher) {
+		//出版社で検索
+		return null;
+	}
+
+
+//ソート
+	public BookBean sortBook(Date pubdate) {
+		// 登録日ソート
+		return null;
+	}
+
+
+//削除
+	public BookBean deleteBook(long isbn) {
+		// 資料を削除
+		return null;
+	}
+
+
+
+
+
 
 
 	private void getConnection() throws DAOException {
@@ -85,5 +164,8 @@ public class BookDAO {
 			con = null;
 		}
 	}
+
+
+
 
 }
--- src/sj/mgr/BookManager.java
+++ src/sj/mgr/BookManager.java
@@ -1,5 +1,6 @@
 package sj.mgr;
 
+import java.sql.Date;
 import java.util.List;
 
 import sj.bean.BookBean;
@@ -7,14 +8,58 @@ import sj.dao.BookDAO;
 import sj.dao.DAOException;
 
 
-public class BookManager {
 
-	// 全てのメンバの情報を取得
-			public List<BookBean> findBookAll() throws DAOException {
+
+public class BookManager {
+	//登録
+			//資料を新規で登録する
+				public BookBean registBook(long isbn, String name, int catcode, String author, String publisher, Date pubdate,
+											String coverurl)throws DAOException {
+					BookDAO dao = new BookDAO();
+					return  dao.insertBook(isbn, name, catcode, author, publisher, pubdate,coverurl);
+				}
+
+			//全てのメンバの情報を取得
+				public List<BookBean> findBookAll() throws DAOException {
+					BookDAO dao = new BookDAO();
+					return dao.findAllBook();
+				}
+	//検索
+			//ISBNで資料を検索する
+				public List<BookBean> searchBookByISBN(long isbn) throws DAOException {
+					BookDAO dao = new BookDAO();
+					return dao.findBookByISBN(isbn);
+				}
+
+			// 名前で資料を検索する
+			public List<BookBean> searchBookByName(String name) throws DAOException {
 				BookDAO dao = new BookDAO();
-				return dao.findAllBook();
+				return dao.findBookByName(name);
+				}
+
+			// 作者で資料を検索する
+			public List<BookBean> searchBookByAuthor(String author) throws DAOException {
+				BookDAO dao = new BookDAO();
+				return dao.findBookByauthor(author);
 			}
 
+			// 出版社で資料を検索する
+			public List<BookBean> searchBookBypublisher(String publisher) throws DAOException {
+				BookDAO dao = new BookDAO();
+				return dao.findBookBypublisher(publisher);
+			}
+	//ソート
+			//登録日順にソート
+			public BookBean sortBook(Date pubdate) throws DAOException {
+				BookDAO dao = new BookDAO();
+				return dao.sortBook(pubdate);
+			}
+	//削除
+			//登録済みの資料を削除
+			public BookBean deleteBook(long isbn) throws DAOException{
+				BookDAO dao = new BookDAO();
+				return dao.deleteBook(isbn);
+			}
 
 
 }
